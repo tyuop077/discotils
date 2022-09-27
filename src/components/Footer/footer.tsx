6bbb5634a6affbb8de5f7368729d7945048012e0
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-09-28 02:47:11 +0300

    Forced font swap & reduced footer spaces

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,11 +1,11 @@
 import styles from "./footer.module.scss";
 
 const Footer = () => (
-    <footer className={styles.footer}>
-        <div className={styles.container}>
-            Discord Utilities • Not affiliated with Discord.
-        </div>
-    </footer>
+  <footer className={styles.footer}>
+    <div className={styles.container}>
+      Discord Utilities • Not affiliated with Discord.
+    </div>
+  </footer>
 )
 
 export default Footer
