811aca99149eaefcb626762894fdaa88ae139050
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-09-28 18:02:33 +0300

    Added loader component

---
+++ src/components/Loader/loader.tsx
@@ -0,0 +1,11 @@
+import styles from "./Loader.module.scss";
+
+const Loader = () => (
+    <div className={styles.loader}>
+        <svg viewBox="25 25 50 50">
+            <circle cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10" />
+        </svg>
+    </div>
+)
+
+export default Loader
