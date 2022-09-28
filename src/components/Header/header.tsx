d6650fef091b40c9a7933a850b461bc7aed3820d
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-09-28 04:02:57 +0300

    Added slash svg & nav buttons

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -2,17 +2,27 @@ import styles from "./header.module.scss";
 import Link from "next/link";
 import Logo from "assets/Logo.svg";
 import Lookup from "assets/Lookup.svg";
+import Slash from "assets/Slash.svg";
 
 const Header = () => (
   <>
     <div className={styles.glow} />
     <header className={styles.header}>
       <div className={styles.container}>
-        <Logo className={styles.logo} />
-        <a href="#"><span>Disco</span>tils</a>
-        <Link href="/lookup">
-          <Lookup />
+        <Link href="/">
+          <div className={styles.logo}>
+            <Logo className={styles.logo} />
+            <a><span>Disco</span>tils</a>
+          </div>
         </Link>
+        <div className={styles.nav}>
+          <Link href="/lookup">
+            <Lookup title="Discord Snowflake Parser & User ID Fetcher" />
+          </Link>
+          <Link href="/application-commands">
+            <Slash title="Application Commands" />
+          </Link>
+        </div>
       </div>
     </header>
   </>
