8ba0ec94d941af0fe21f913c2cd2991d58a952a2
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-10-16 00:45:09 +0300

    Changed commands and guilds util classes to custom hooks

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,11 +1,18 @@
 import AccountManager from "@utils/accountManager";
 import Command, {ICommand} from "@utils/command";
+import useSWR from "swr";
+import {fetcher} from "@utils/fetcher";
 
-export default class Commands {
-  static async getCommands(applicationId: string, guildId: string): Promise<Command[]> {
-    const account = AccountManager.accounts[applicationId];
-    const res = await account.rest(`applications${guildId === "global" ? `/guilds/${guildId}` : ""}/${applicationId}/commands?with_localizations=true`);
-    const json: ICommand[] = await res.json();
-    return json.map(c => new Command(c));
+export const useCommands = (applicationId?: string, guildId?: string) => {
+  if (!applicationId) throw new Error("No application id provided");
+  if (!guildId) throw new Error("No guild id provided");
+  const {data, error} = useSWR([
+    `applications${guildId === "global" ? `/guilds/${guildId}` : ""}/${applicationId}/commands?with_localizations=true`,
+    AccountManager.accounts[applicationId].token
+  ], fetcher);
+  return {
+    commands: (data as ICommand[])?.map(c => new Command(c)),
+    isLoading: !error && !data,
+    isError: error
   }
 }
--- src/utils/guilds.ts
+++ src/utils/guilds.ts
@@ -1,9 +1,14 @@
+import useSWR from 'swr';
+import {fetcher} from "@utils/fetcher";
 import AccountManager from "@utils/accountManager";
-import Guild from "@utils/guild";
+import Guild, {IGuild} from "@utils/guild";
 
-export class Guilds {
-  static async getGuilds(accountId: string) {
-    const res = await AccountManager.accounts[accountId].rest(`users/@me/guilds`);
-    if (res.ok) return (await res.json() as []).map(g => new Guild(g));
+export const useGuilds = (accountId?: string) => {
+  if (!accountId) throw new Error("No account id provided");
+  const {data, error} = useSWR([`users/@me/guilds`, AccountManager.accounts[accountId].token], fetcher);
+  return {
+    guilds: (data as IGuild[])?.map(g => new Guild(g)),
+    isLoading: !error && !data,
+    isError: error
   }
 }
