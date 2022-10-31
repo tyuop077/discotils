8df788de6f20775763f97cc0c5f04a4c23fa401f
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-01 00:02:15 +0300

    Added add and refresh buttons to accounts page

--- src/pages/accounts.tsx
+++ src/pages/accounts.tsx
@@ -2,6 +2,9 @@ import {NextPage} from "next";
 import styles from "@styles/Accounts.module.scss";
 import Head from "next/head";
 import AccountList from "@components/AccountList/accountList";
+import Plus from "@assets/Plus.svg";
+import Refresh from "@assets/Refresh.svg";
+import Link from "next/link";
 
 const Accounts: NextPage = () => {
   return (
@@ -11,6 +14,18 @@ const Accounts: NextPage = () => {
         <meta name="description" content="Discord Utilities" />
       </Head>
       <h1>Accounts</h1>
+      <div className={styles.buttons}>
+        <button className={styles.button}>
+          <Refresh />
+          Refresh
+        </button>
+        <Link href="/accounts/new">
+          <button className={styles.button}>
+            <Plus />
+            Add
+          </button>
+        </Link>
+      </div>
       <AccountList extended />
     </main>
   )
