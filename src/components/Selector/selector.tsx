ec45d109815e6fafe1dabccc197900cc59b4ddb6
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-05 03:44:41 +0300

    Updated selector component's card description

--- src/components/Selector/selector.tsx
+++ src/components/Selector/selector.tsx
@@ -5,7 +5,7 @@ import Slash from "@assets/Slash.svg";
 
 const Selector = () => (
   <div className={styles.selector}>
-    <SelectorCard title="Lookup" href="/lookup" icon={<Lookup />} label="Snowflake parser and user ID resolver" />
+    <SelectorCard title="Lookup" href="/lookup" icon={<Lookup />} label="Snowflake parser, user and server ID resolver" />
     <SelectorCard title="Application Commands" href="/commands" icon={<Slash />}
                   label="Slash commands and context menus editor" />
   </div>
