829269ee8894a1072d6c961f06b7c7268dfdd6f9
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-08 01:04:09 +0300

    Created lookup layout

---
+++ src/components/layout/Lookup/lookup.tsx
@@ -0,0 +1,61 @@
+import {MutableRefObject, ReactElement, useEffect, useRef, useState} from "react";
+import styles from "./lookup.module.scss";
+import SnowflakeParser from "@components/SnowflakeParser/snowflakeParser";
+import LookupLink from "@components/LookupLink/lookupLink";
+import {useRouter} from "next/router";
+
+const LookupLayout = (page: ReactElement) => {
+  const router = useRouter();
+  const [id, setId] = useState<string>("");
+  const valid = /^\d{17,20}$/.test(id);
+  const inputRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const initialId = router.query.id;
+  useEffect(() => {
+    if (typeof initialId !== "string") return;
+    setId(initialId);
+    inputRef.current!.value = initialId;
+  }, [initialId]);
+  return (
+    <main className={styles.container}>
+      <h1>Snowflake parser & ID resolver</h1>
+      <h2>Enter the ID of the user or server to get information about it</h2>
+      <div className={styles.input}>
+        <input
+          type="text"
+          placeholder="ID"
+          ref={inputRef}
+          onChange={(e) => {
+            const value = e.target.value;
+            router.replace(`${router.pathname}?id=${value}`, router.pathname, {
+              shallow: true
+            });
+            setId(value);
+          }}
+        />
+        <div className={styles.type}>
+          <LookupLink href="" activeClass={styles.active}>
+            Snowflake
+          </LookupLink>
+          <LookupLink href="/user" activeClass={styles.active}>
+            User
+          </LookupLink>
+          <LookupLink href="/guild" activeClass={styles.active}>
+            Server
+          </LookupLink>
+        </div>
+      </div>
+      {valid && (
+        <>
+          <div className={styles.results}>
+            <SnowflakeParser id={id} />
+          </div>
+          <div className={styles.results}>
+            {page}
+          </div>
+        </>
+      )}
+    </main>
+  );
+}
+
+export default LookupLayout
---
+++ src/components/ui/LookupLink/lookupLink.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+
+interface Props {
+  children: React.ReactNode;
+  activeClass: string;
+  href: string;
+}
+
+const LookupLink = ({children, activeClass, href}: Props) => {
+  const router = useRouter();
+  return (
+    <Link href={`/lookup${href}`} className={router.pathname == `/lookup${href}` ? activeClass : undefined} scroll={false}>
+      {children}
+    </Link>
+  )
+}
+
+export default LookupLink
