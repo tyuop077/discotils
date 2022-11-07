a14f1597023504c43b8f1b187080bc66ccc2ea5e
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-08 01:05:36 +0300

    Added minimize button to snowflake parser component

--- src/components/ui/SnowflakeParser/snowflakeParser.tsx
+++ src/components/ui/SnowflakeParser/snowflakeParser.tsx
@@ -20,105 +20,118 @@ const SnowflakeParser = ({id}: {id: string}) => {
 
   const [type, setType] = useState<DateType>({lang: "default", dateStyle: "full", timeStyle: "long"});
   const [show, setShow] = useState(false);
+  const [maximized, setMaximized] = useState(true);
   const str = new Intl.DateTimeFormat(type.lang, { dateStyle: type.dateStyle, timeStyle: type.timeStyle, timeZone: type.timezone }).format(Number(data.timestamp));
 
   return (
     <div className={styles.snowflake}>
-      <p>Timestamp: </p>
-      <p className={styles.timestamp}>
-        <span>{Number(data.timestamp) / 1000 | 0}</span>
-        <small>{String(Number(data.timestamp) % 1000 | 0).padStart(3, "0")}</small>
-      </p>
-      <p>Created on: </p>
-      <span className={styles.date}>
-        {data.date.toLocaleString()}
-        <CopyToClipboard text={data.timestamp} />
-      </span>
-      <div className={styles.string}>
-        <span>{str}</span>
-        <ArrowDown
-          className={show ? styles.open : undefined}
-          onClick={() => setShow(!show)}
-        />
+      <div className={`${styles.head}${maximized ? "" : ` ${styles.hidden}`}`}>
+        <h4>{maximized ? "Snowflake" : `Snowflake (created on ${data.date.toLocaleString()})`}</h4>
+        <button
+          onClick={() => setMaximized(!maximized)}
+        >
+          <ArrowDown className={maximized ? undefined : styles.hidden} />
+        </button>
       </div>
-      {show && (
-        <div className={styles.options}>
-          <div className={styles.option}>
-            <span>Language</span>
-            <div>
-              <button
-                className={type.lang === "default" ? styles.selected : undefined}
-                onClick={() => setType({...type, lang: "default"})}
-              >
-                Local
-              </button>
-              <button
-                className={type.lang === "en-US" ? styles.selected : undefined}
-                onClick={() => setType({...type, lang: "en-US"})}
-              >
-                English
-              </button>
-            </div>
+      {maximized ? (
+        <>
+          <p>Timestamp: </p>
+          <p className={styles.timestamp}>
+            <span>{Number(data.timestamp) / 1000 | 0}</span>
+            <small>{String(Number(data.timestamp) % 1000 | 0).padStart(3, "0")}</small>
+          </p>
+          <p>Created on: </p>
+          <span className={styles.date}>
+        {data.date.toLocaleString()}
+            <CopyToClipboard text={data.timestamp} />
+      </span>
+          <div className={styles.string}>
+            <span>{str}</span>
+            <ArrowDown
+              className={show ? styles.open : undefined}
+              onClick={() => setShow(!show)}
+            />
           </div>
-          <div className={styles.option}>
-            <span>Timezone</span>
-            <div>
-              <button
-                className={type.timezone === undefined ? styles.selected : undefined}
-                onClick={() => setType({...type, timezone: undefined})}
-              >
-                Local
-              </button>
-              <button
-                className={type.timezone === "Etc/UTC" ? styles.selected : undefined}
-                onClick={() => setType({...type, timezone: "Etc/UTC"})}
-              >
-                UTC
-              </button>
+          {show && (
+            <div className={styles.options}>
+              <div className={styles.option}>
+                <span>Language</span>
+                <div>
+                  <button
+                    className={type.lang === "default" ? styles.selected : undefined}
+                    onClick={() => setType({...type, lang: "default"})}
+                  >
+                    Local
+                  </button>
+                  <button
+                    className={type.lang === "en-US" ? styles.selected : undefined}
+                    onClick={() => setType({...type, lang: "en-US"})}
+                  >
+                    English
+                  </button>
+                </div>
+              </div>
+              <div className={styles.option}>
+                <span>Timezone</span>
+                <div>
+                  <button
+                    className={type.timezone === undefined ? styles.selected : undefined}
+                    onClick={() => setType({...type, timezone: undefined})}
+                  >
+                    Local
+                  </button>
+                  <button
+                    className={type.timezone === "Etc/UTC" ? styles.selected : undefined}
+                    onClick={() => setType({...type, timezone: "Etc/UTC"})}
+                  >
+                    UTC
+                  </button>
+                </div>
+              </div>
+              <div className={styles.option}>
+                <span>Date style</span>
+                <div>
+                  <button
+                    className={type.dateStyle === "full" ? styles.selected : undefined}
+                    onClick={() => setType({...type, dateStyle: "full"})}
+                  >
+                    Full
+                  </button>
+                  <button
+                    className={type.dateStyle === "short" ? styles.selected : undefined}
+                    onClick={() => setType({...type, dateStyle: "short"})}
+                  >
+                    Short
+                  </button>
+                </div>
+              </div>
+              <div className={styles.option}>
+                <span>Time style</span>
+                <div>
+                  <button
+                    className={type.timeStyle === "long" ? styles.selected : undefined}
+                    onClick={() => setType({...type, timeStyle: "long"})}
+                  >
+                    Long
+                  </button>
+                  <button
+                    className={type.timeStyle === "short" ? styles.selected : undefined}
+                    onClick={() => setType({...type, timeStyle: "short"})}
+                  >
+                    Short
+                  </button>
+                  <button
+                    className={type.timeStyle === "full" ? styles.selected : undefined}
+                    onClick={() => setType({...type, timeStyle: "full"})}
+                  >
+                    Full
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className={styles.option}>
-            <span>Date style</span>
-            <div>
-              <button
-                className={type.dateStyle === "full" ? styles.selected : undefined}
-                onClick={() => setType({...type, dateStyle: "full"})}
-              >
-                Full
-              </button>
-              <button
-                className={type.dateStyle === "short" ? styles.selected : undefined}
-                onClick={() => setType({...type, dateStyle: "short"})}
-              >
-                Short
-              </button>
-            </div>
-          </div>
-          <div className={styles.option}>
-            <span>Time style</span>
-            <div>
-              <button
-                className={type.timeStyle === "long" ? styles.selected : undefined}
-                onClick={() => setType({...type, timeStyle: "long"})}
-              >
-                Long
-              </button>
-              <button
-                className={type.timeStyle === "short" ? styles.selected : undefined}
-                onClick={() => setType({...type, timeStyle: "short"})}
-              >
-                Short
-              </button>
-              <button
-                className={type.timeStyle === "full" ? styles.selected : undefined}
-                onClick={() => setType({...type, timeStyle: "full"})}
-              >
-                Full
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+          )}
+        </>
+      ) : null}
     </div>
   )
 }
