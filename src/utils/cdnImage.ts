9e4c25f9f64569dc2e41fa427941c668fba33a67
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-16 02:24:19 +0300

    Changed cdn image util to force-set avatars path on undefined hash

--- src/utils/cdnImage.ts
+++ src/utils/cdnImage.ts
@@ -6,5 +6,5 @@ interface ImageOptions {
 export const cdnImage = (path: string, size = 128, id: string, hash?: string, options: ImageOptions = {}) =>
   "https://cdn.discordapp.com/" + (hash ?
       `${path}/${id}/${hash}.${hash.startsWith("a_") ? "gif" : options.format ?? "webp"}?size=${size}` :
-      `embed/${path}/${options.discriminator ? (Number(options.discriminator) % 5) : 0}.png`
+      `embed/${hash ? path : "avatars"}/${options.discriminator ? (Number(options.discriminator) % 5) : 0}.png`
   );
