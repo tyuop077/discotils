9cb627a29de4b7a49cb3eb656bd0ca6ca43ea98c
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-15 20:18:33 +0300

    Added lookup guild component

---
+++ src/components/ui/LookupGuild/lookupGuild.tsx
@@ -0,0 +1,53 @@
+import styles from "./lookupUser.module.scss";
+import useSWRImmutable from "swr/immutable";
+import {CodeToLine, RestForwarderError} from "@utils/restForwarderHandler";
+import {fetcherWithStatus, WithStatus} from "@utils/fetcher";
+import Loader from "@components/Loader/loader";
+import CloudOff from "@assets/CloudOff.svg";
+import UserOff from "@assets/UserOff.svg";
+import {GuildPreview, GuildWidget, Invite} from "@utils/discordTypes";
+
+const LookupGuild = ({id}: {id: string}) => {
+  const {data: widgetData, error: widgetError} = useSWRImmutable<WithStatus<GuildWidget | RestForwarderError>>(
+    `https://discord.com/api/guilds/${id}/widget.json`, fetcherWithStatus
+  );
+  const {data: inviteData, error: inviteError} = useSWRImmutable<WithStatus<Invite | RestForwarderError>>(
+    (widgetData?.body as GuildWidget)?.instant_invite ?
+      `https://discord.com/api/v10/invites/${(widgetData?.body as GuildWidget).instant_invite}` +
+      "?with_counts=true&with_expiration=true" : null,
+    fetcherWithStatus
+  );
+  const {data: previewData, error: previewError} = useSWRImmutable<WithStatus<GuildPreview | RestForwarderError>>(
+    (widgetData?.body as GuildWidget).id ? `/api/guild/${id}/preview` : null, fetcherWithStatus
+  );
+  return (
+    widgetData ? (
+      widgetData.status === 200 ? (
+        // TODO: Add guild profile
+        <GuildCard
+          widget={widgetData.body as GuildWidget}
+          invite={inviteData?.body as Invite}
+          preview={previewData?.body as GuildPreview}
+        />
+      ) : (
+        <div className={styles.error}>
+          {widgetData.status === 404 ? <UserOff /> : <CloudOff />}
+          <h3>
+            {CodeToLine[widgetData.status](widgetData.body as RestForwarderError) ?? (widgetData.body as RestForwarderError).error}
+          </h3>
+        </div>
+      )
+    ) : widgetError ? (
+      <div className={styles.error}>
+        <CloudOff />
+        <h3>
+          Looks like there&apos;s a connection issue, please check your access to the internet and try again
+        </h3>
+      </div>
+    ) : (
+      <Loader />
+    )
+  );
+}
+
+export default LookupGuild;
