536e8c693a5fab907856c30de8ae79b573dee6e3
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-15 19:14:23 +0300

    Created guild preview api endpoint

---
+++ src/pages/api/guild/[id]/preview.ts
@@ -0,0 +1,10 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {restForwarder} from "@utils/restForwarder";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  return restForwarder(req, res, {
+    path: id => `guilds/${id}/preview`,
+    rateLimitBucket: "guildsPreview",
+    entityName: "Guild"
+  })
+}
