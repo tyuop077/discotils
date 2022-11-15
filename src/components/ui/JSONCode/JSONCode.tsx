28b914b322c02639e980c7372845d2a91a655126
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-15 18:46:51 +0300

    Added JSONCode component

---
+++ src/components/ui/JSONCode/JSONCode.tsx
@@ -0,0 +1,30 @@
+import styles from "./JSONCode.module.scss"
+import {CopyToClipboard} from "@components/CopyToClipboard/copyToClipboard";
+import External from "@assets/External.svg";
+
+export const JSONCode = ({content, title}: {content: any, title?: string}) => {
+  return (
+    <div className={styles.code}>
+      <div className={styles.content}>
+        <div className={styles.header}>
+          <code>{title ?? "JSON"}</code>
+        </div>
+        <code>{JSON.stringify(content)}</code>
+      </div>
+      <div className={styles.tools}>
+        <button
+          className={styles.external}
+          onClick={() => {
+            window.open(
+              `https://jsoneditoronline.org/#left=json.${encodeURIComponent(JSON.stringify(content))}`,
+              "_blank"
+            );
+          }}
+        >
+          <External />
+        </button>
+        <CopyToClipboard text={JSON.stringify(content)} />
+      </div>
+    </div>
+  )
+}
