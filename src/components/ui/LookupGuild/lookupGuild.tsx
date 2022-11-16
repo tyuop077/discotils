2a09afdd45d3ca9cf47cb8d9304b124dc4d54332
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-17 00:38:36 +0300

    Added ability to provide guild invite code if known

--- src/components/ui/LookupGuild/lookupGuild.tsx
+++ src/components/ui/LookupGuild/lookupGuild.tsx
@@ -9,29 +9,42 @@ import {GuildPreview, GuildWidget, Invite} from "@utils/discordTypes";
 import Warning from "@assets/Warning.svg";
 import {GuildCard} from "@components/LookupGuild/guildCard";
 import {Snowflake} from "@utils/snowflake";
+import {useState} from "react";
 
 const LookupGuild = ({id}: {id: string}) => {
   const valid = /^\d{17,20}$/.test(id) && Snowflake.toTimestamp(id) + BigInt(10000) <= Date.now();
+  const [customInviteCode, setCustomInviteCode] = useState<string>();
   const {data: widgetData, error: widgetError} = useSWRImmutable<WithStatus<GuildWidget | RestForwarderError>>(
     valid ? `https://discord.com/api/guilds/${id}/widget.json` : null,
     fetcherWithStatus
   );
+  const inviteCode = customInviteCode || (widgetData?.body as GuildWidget)?.instant_invite?.split("/").at(-1);
   const {data: inviteData, error: inviteError} = useSWRImmutable<WithStatus<Invite | RestForwarderError>>(
-    (widgetData?.body as GuildWidget)?.instant_invite ?
-      "https://discord.com/api/v10/invites/" +
-      (widgetData?.body as GuildWidget).instant_invite?.split("/").at(-1) +
-      "?with_counts=true&with_expiration=true" : null,
+    inviteCode ? `https://discord.com/api/v10/invites/${inviteCode}?with_counts=true&with_expiration=true` : null,
     fetcherWithStatus
   );
   const {data: previewData, error: previewError} = useSWRImmutable<WithStatus<GuildPreview | RestForwarderError>>(
-    (widgetData?.status === 200 || widgetData?.status === 403) ?
-      `/api/guild/${id}/preview` : null,
+    (widgetData?.status === 200 || widgetData?.status === 403) ? `/api/guild/${id}/preview` : null,
     fetcherWithStatus
   );
   return (
     widgetData ? (
       (widgetData.status === 200 || widgetData.status === 403) ? (
         <>
+          {(customInviteCode && (id !== (inviteData?.body as Invite)?.guild.id)) && (
+            <div className={styles.note}>
+              <Warning />
+              <p>
+                Provided code &quot;{customInviteCode}&quot; is not an invite for this guild.
+                Expected {id}, got {(inviteData?.body as Invite)?.guild.id}
+                <button
+                  onClick={() => setCustomInviteCode(undefined)}
+                >
+                  Clear code
+                </button>
+              </p>
+            </div>
+          )}
           {(inviteError || previewError) && (
             <div className={styles.note}>
               <Warning />
@@ -56,12 +69,28 @@ const LookupGuild = ({id}: {id: string}) => {
             invite={inviteData?.body as Invite}
             preview={previewData?.body as GuildPreview}
           />
+          {widgetData.status !== 200 && (
+            <div className={`${styles.note} ${styles.info}`}>
+              <p>
+                <span>BETA</span> You can provide an invite code or vanity URL if you have one:
+                <input
+                  type="text"
+                  placeholder="discord-developers"
+                  onKeyDown={e => {
+                    if (e.key !== "Enter") return;
+                    setCustomInviteCode(e.currentTarget.value.split("/").at(-1));
+                  }}
+                />
+              </p>
+            </div>
+          )}
         </>
       ) : (
         <div className={styles.error}>
           {widgetData.status === 404 ? <Logo /> : <CloudOff />}
           <h3>
-            {DiscordLocalizedStatus[widgetData.status](widgetData.body as RestForwarderError, "Guild") ?? (widgetData.body as RestForwarderError).error}
+            {DiscordLocalizedStatus[widgetData.status](widgetData.body as RestForwarderError, "Guild") ??
+              (widgetData.body as RestForwarderError).error}
           </h3>
         </div>
       )
