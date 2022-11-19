a70cfcbc1939c926d4bc68e443dc4182b166b707
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-20 01:36:09 +0300

    Added GitHub link into header component

--- src/components/ui/Header/header.tsx
+++ src/components/ui/Header/header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Logo from "@assets/Logo.svg";
 import Lookup from "@assets/Lookup.svg";
 import Slash from "@assets/Slash.svg";
+import GitHub from "@assets/GitHub.svg";
 
 const Header = () => (
   <>
@@ -19,6 +20,11 @@ const Header = () => (
           </Link>
           <Link href="/commands" title="Application Commands">
             <Slash />
+          </Link>
+        </div>
+        <div className={styles.navR}>
+          <Link href="https://github.com/tyuop077/discotils" title="GitHub">
+            <GitHub />
           </Link>
         </div>
       </div>
