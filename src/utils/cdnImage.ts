a85893633ea0aff23f5de0bf9183002497ff3f43
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2022-11-20 02:21:54 +0300

    Updated cdnImage util to support emojis and stickers

--- src/utils/cdnImage.ts
+++ src/utils/cdnImage.ts
@@ -1,10 +1,12 @@
 interface ImageOptions {
   discriminator?: string;
   format?: string;
+  noHash?: boolean;
 }
 
-export const cdnImage = (path: string, size = 128, id: string, hash?: string, options: ImageOptions = {}) =>
-  "https://cdn.discordapp.com/" + (hash ?
-      `${path}/${id}/${hash}.${hash.startsWith("a_") ? "gif" : options.format ?? "webp"}?size=${size}` :
+export const cdnImage = (path: string, size = 128, id: string, hash?: string | null, options: ImageOptions = {}) =>
+  "https://cdn.discordapp.com/" + (hash || options.noHash ?
+      [path, id, options.noHash ? "" : hash].filter(Boolean).join("/") +
+      `.${hash?.startsWith("a_") ? "gif" : options.format ?? "webp"}?size=${size}` :
       `embed/${hash ? path : "avatars"}/${options.discriminator ? (Number(options.discriminator) % 5) : 0}.png`
   );
